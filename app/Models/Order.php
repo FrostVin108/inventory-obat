2a82c64258a4bca1f87758df12d153aa6a1c832f
FrostVin108 <vincentsidarta.hu@gmail.com>
2024-09-19 09:37:21 +0700

    order fixing

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,7 +9,7 @@ class Order extends Model
 {
     use HasFactory;
 
-    protected $table = 'Order';
+    protected $table = 'order';
     protected $primaryKey = 'id';
 
     protected $fillable = [
