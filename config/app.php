050dec4252edea207760673f4c140d9431605ead
FrostVin108 <vincentsidarta.hu@gmail.com>
2024-09-20 15:53:46 +0700

    controller pdf update

--- config/app.php
+++ config/app.php
@@ -163,12 +163,14 @@ return [
         /*
          * Application Service Providers...
          */
+        
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
     ])->toArray(),
 
     /*
