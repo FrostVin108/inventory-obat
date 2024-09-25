4957aaa146b954f1db89a7a97fa7f786c4d35bc3
FrostVin108 <82022923+FrostVin108@users.noreply.github.com>
2024-09-25 10:46:42 +0700

    Update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $this->call(UserSeeder::class);
         $this->call(DepartmentSeeder::class);
         // \App\Models\User::factory(10)->create();
 
